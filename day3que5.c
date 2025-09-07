22bd9487cb0fd255834cea62d57a4cf19c10272e
Subhasmita swain <khushiswain712@gmail.com>
2025-09-07 18:41:31 +0530

    Create day3que5.c

---
+++ day3que5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+int f,c,temperature;
+printf("Enter 
+  temperature in celcius: ");
+scanf("%d",&c);
+f=(c*9/5)+32;
+printf("Temperature in fahrenheit is: %d",f);
+return 0;
+}
