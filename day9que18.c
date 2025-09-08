3fcae1b449dc9a37a0524ac446caade6cf0f6237
Subhasmita swain <khushiswain712@gmail.com>
2025-09-08 22:43:43 +0530

    Create day9que18.c

---
+++ day9que18.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+int percentage;
+printf(" enter your percentage :");
+scanf("%d",&percentage);
+if(percentage>=90 && percentage<=100)
+{ printf(" Grade A");}
+else if(percentage>=80 && percentage<+89)
+{printf(" Grade B");}
+else if(percentage>=70 && percentage<=79)
+{ printf(" Grade C");}
+else if(percentage>=60 && percentage<=69)
+{ printf("Grade D");}
+else if (percentage<60)
+{ printf("Grade F");}
+else
+{ printf(" invalid percentage");}
+return 0;
+}
+
+
