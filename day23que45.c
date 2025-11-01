a72cce193381c6d28833ec289192158af8f30c19
Subhasmita swain <khushiswain712@gmail.com>
2025-11-02 00:00:31 +0530

    Update day23que45.c

--- day23que45.c
+++ day23que45.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    float sum = 0.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        float numerator = 2 * i;
+        float denominator = 4 * i - 1;
+        sum += numerator / denominator;
+    }
+
+    printf("Sum of the series = %.2f\n", sum);
+    return 0;
+}
