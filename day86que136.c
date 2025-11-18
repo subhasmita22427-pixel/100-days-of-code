47a88dd7628392d2a352d6c81265a9d0ef5404ec
Subhasmita swain <khushiswain712@gmail.com>
2025-11-18 21:09:52 +0530

    Create day86que136.c

---
+++ day86que136.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+enum Menu 
+{
+    ADD = 1,
+    SUBTRACT,
+    MULTIPLY
+};
+int main() 
+{
+    char op[20];
+    int a, b;
+    enum Menu choice;
+    scanf("%s %d %d", op, &a, &b);
+    if (strcmp(op, "ADD") == 0)
+        choice = ADD;
+    else if (strcmp(op, "SUBTRACT") == 0)
+        choice = SUBTRACT;
+    else if (strcmp(op, "MULTIPLY") == 0)
+        choice = MULTIPLY;
+    else {
+        printf("Invalid Operation\n");
+        return 0;
+    }
+    switch (choice) 
+    { case ADD:
+            printf("%d\n", a + b);
+            break;
+
+        case SUBTRACT:
+            printf("%d\n", a - b);
+            break;
+
+        case MULTIPLY:
+            printf("%d\n", a * b);
+            break;
+    }
+    return 0;
+}
